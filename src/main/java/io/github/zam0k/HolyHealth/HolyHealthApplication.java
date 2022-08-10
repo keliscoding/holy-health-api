57cdb231719b1caab0d4a6eaaff8221fce07f029
Kelly Castelo <kawokkun@gmail.com>
2022-08-10 07:48:57 -0300

    Initial commit

---
+++ src/main/java/io/github/zam0k/HolyHealth/HolyHealthApplication.java
@@ -0,0 +1,13 @@
+package io.github.zam0k.HolyHealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolyHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolyHealthApplication.class, args);
+	}
+
+}
